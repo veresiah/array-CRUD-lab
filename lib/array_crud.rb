aa6d2b07056391abe14a01ef3dffbc3735762d89
Veresia Hiralall <hiralallveresia@yahoo.com>
2020-10-31 16:03:34 -0400

    commit tree bffe0cca183f9d6e13041c190a2f44f6a287a6d9

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,35 +1,37 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
-  
+  colors = ["red", "white", "yellow", "blue"]
 end
 
-def add_element_to_end_of_array(array, element)
-  
+def add_element_to_end_of_array(array, element) 
+  array << "#{element}"
+  array << "arrays!"
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  array.unshift ("#{element}")
+  array.unshift ("wow")
 end
 
 def remove_element_from_end_of_array(array)
-  
+  array.pop 
 end
 
 def remove_element_from_start_of_array(array)
-  
+  array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+  array [index_number]
 end
 
 def retrieve_first_element_from_array(array)
-  
+  array[0]
 end
 
 def retrieve_last_element_from_array(array)
-  
+  array[-1]
 end
